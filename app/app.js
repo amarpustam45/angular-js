44eca95a8ff453ee8add6b27407f617e3d6d5dfe
amarpustam45 <amarpustam@hotmail.com>
2022-05-02 20:14:19 -0400

    ng-src

--- app/app.js
+++ app/app.js
@@ -5,26 +5,48 @@ myApp.controller('appController', [
   ($scope) => {
     $scope.message = 'hello world';
 
+    $scope.removeNinja = (ninja) => {
+      const removedNinja = $scope.ninjas.indexOf(ninja);
+      $scope.ninjas.splice(removedNinja, 1);
+    };
+
+    $scope.addNinja = () => {
+      $scope.ninjas.push({
+        name: $scope.newninja.name,
+        belt: $scope.newninja.belt,
+        rate: parseInt($scope.newninja.rate),
+        avaliable: true,
+      });
+
+      $scope.newninja.name = '';
+      $scope.newninja.belt = '';
+      $scope.newninja.rate = '';
+    };
+
     $scope.ninjas = [
       {
         name: 'amar',
         belt: 'green',
         rate: 50,
-      },
-      {
-        name: 'ryuk',
-        belt: 'yellow',
-        rate: 30,
-      },
-      {
-        name: 'random',
-        belt: 'blue',
-        rate: 60,
+        avaliable: true,
       },
       {
         name: 'someone',
         belt: 'red',
         rate: 20,
+        avaliable: true,
+      },
+      {
+        name: 'ryuk',
+        belt: 'yellow',
+        rate: 30,
+        avaliable: true,
+      },
+      {
+        name: 'random',
+        belt: 'blue',
+        rate: 60,
+        avaliable: false,
       },
     ];
   },
